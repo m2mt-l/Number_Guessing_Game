b7cd581d58510d4ef433c44bd777064e895833ac
m2mt-l <ta28ma2@gmail.com>
2022-05-20 01:33:27 +0000

    feat: add ngif for playertwo

---
+++ src/app/config.ts
@@ -0,0 +1,9 @@
+export interface Config {
+    minimumNumber: number;
+    maximumNumber: number;
+}
+
+const defaultValue: Config = {
+    minimumNumber: 1,
+    maximumNumber: 100,
+};
--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { Config } from '../config';
 
 @Component({
-  selector: 'app-game',
-  templateUrl: './game.component.html',
-  styleUrls: ['./game.component.css']
+    selector: 'app-game',
+    templateUrl: './game.component.html',
+    styleUrls: ['./game.component.css'],
 })
 export class GameComponent implements OnInit {
+    constructor() {}
 
-  constructor() { }
+    sentencePlayerOne: string = 'Player one enter a value between 1 and 100 to be guessed';
+    sentencePlayerTwo: string = 'Player two enter a value between 1 and 100 to be guessed';
+    isPlayerOne: boolean = true;
+    counter: number = 1;
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {}
 
+    onSelect(n: string): void {
+        this.isPlayerOne = false;
+        console.log(n);
+    }
 }
